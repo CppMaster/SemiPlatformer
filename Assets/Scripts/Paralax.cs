8d8eb1239c947f16f8df2f85c004c5fb5b210076
Patryk Steckel <patryksteckel@gmail.com>
2016-01-30 20:04:10 +0100

    Paralax

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Paralax : MonoBehaviour {
+
+	public Transform[] layers;
+    public float[] moveRatio;
+    Vector3[] normalPositions;
+    public float lerpSpeed = 1f;
+
+    public Transform player;
+
+    void Start()
+    {
+        normalPositions = new Vector3[layers.Length];
+        for(int a = 0; a < layers.Length; ++a)
+        {
+            normalPositions[a] = layers[a].position;
+        }
+    }
+
+	void Update ()
+    {
+	    for(int a = 0; a < layers.Length; ++a)
+        {
+            layers[a].position = Vector3.Lerp(layers[a].position, normalPositions[a] + Vector3.right * player.position.x * moveRatio[a], lerpSpeed * Time.deltaTime);
+        }
+	}
+}
