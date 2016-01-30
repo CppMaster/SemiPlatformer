b49f5ac6304a4b9bbeeeaea59a60251095079974
Patryk Steckel <patryksteckel@gmail.com>
2016-01-30 19:23:05 +0100

    Kick animation

--- Assets/Scripts/Characters/ComboPuncher.cs
+++ Assets/Scripts/Characters/ComboPuncher.cs
@@ -33,8 +33,8 @@ public class ComboPuncher : Puncher {
             }
         }
 
-        //if (animator != null)
-        //    animator.SetInteger("currentPunch", currentNode);
+        if (animator != null)
+            animator.SetInteger("CurrentPunch", currentNode);
     }
 
     int GetNextNode(int node, int edge)
