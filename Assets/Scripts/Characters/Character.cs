5a859de3987d1866e5b8aa36e889dc8603c8dbdc
Dariusz Szylak <dariusz.szylak@logicims.com>
2016-01-31 02:28:24 +0100

    Minor fix

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -47,9 +47,6 @@ public class Character : Lootable
     public override void Die()
     {
 
-        if (lootItems.Length > 0)
-            Instantiate(lootItems[Random.Range(0, lootItems.Length)], transform.position, Quaternion.identity);
-
         if (animator != null)
         {
             animator.SetTrigger("Dead");
